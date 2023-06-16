61cb4b64576d0a88281fbbafd9d7def499df71e6
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2023-06-16 22:31:29 +0200

    Update servo_motor_controller.py

--- Client/hardware/servo_motor_controller.py
+++ Client/hardware/servo_motor_controller.py
@@ -1,13 +1,32 @@
-from gpiozero import Servo
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-servo = Servo(25)
+
+servoPIN = 25
+# Possible servo positions
+# 2,5 -> cable side
+# 7,5 -> middle
+# 12,5 -> opposed cable side
+unlocked_position = 12.5
+locked_position = 7.5
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+p = GPIO.PWM(servoPIN, 50)          # GPIO as PWM with 50Hz
+
+p.start(locked_position)
+
+
+def set_servo_cycle(position):
+    p.ChangeDutyCycle(position)
+    time.sleep(0.5)
 
 
 def lock_lid():
-    servo.min()
+    set_servo_cycle(locked_position)
     # servo.max()
 
 
 def unlock_lid():
-    servo.mid()
+    set_servo_cycle(unlocked_position)
