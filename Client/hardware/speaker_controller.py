3b886590df7c31e8874866b7b5acd9f7e3fbac76
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2023-06-28 10:52:12 +0200

    implemented LEDs and buzzer

--- Client/hardware/speaker_controller.py
+++ Client/hardware/speaker_controller.py
@@ -1,3 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+
+# Numbering of GPIO Pins not the one on the board
+GPIO.setmode(GPIO.BCM)
+
+buzzer_pin = 24
+
+GPIO.setup(buzzer_pin, GPIO.OUT)
+GPIO.output(buzzer_pin, GPIO.LOW)
+
+
+def beep_beep():
+    GPIO.output(buzzer_pin, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(buzzer_pin, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(buzzer_pin, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(buzzer_pin, GPIO.LOW)
+
 
 def play_sound():
-    ...
+    beep_beep()
+    time.sleep(1)
+    beep_beep()
