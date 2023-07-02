e8b83d8967bd8e4d24337897126e53fa0e422d17
sebastianmeyr <sebastianmeyr@gmx.net>
2023-07-02 11:36:14 +0200

    feat: add qr code scanner to device login

--- App/src/shared/textInput.tsx
+++ App/src/shared/textInput.tsx
@@ -7,29 +7,31 @@ type Props = {
   value: string;
   multiline?: boolean;
   secureTextEntry?: boolean;
+  onFocus?: () => void;
 };
 
 export default function InputField(props: Props) {
   return (
-    <View
+    <TextInput
+      placeholder={props.defaultText}
+      value={props.value}
+      onChangeText={props.onValueChange}
+      secureTextEntry={props.secureTextEntry}
+      multiline={props.multiline}
+      autoCorrect={false}
+      autoComplete={"off"}
+      autoCapitalize="none"
+      onFocus={props.onFocus}
       style={{
+        alignSelf: "stretch",
         borderColor: COLORS.brand,
         borderWidth: 1,
         borderRadius: 20,
+        paddingTop: 12,
         padding: 12,
-        alignSelf: "stretch",
+        alignItems: "center",
+        minHeight: 45,
       }}
-    >
-      <TextInput
-        placeholder={props.defaultText}
-        value={props.value}
-        onChangeText={props.onValueChange}
-        secureTextEntry={props.secureTextEntry}
-        multiline={props.multiline}
-        autoCorrect={false}
-        autoComplete={"off"}
-        autoCapitalize="none"
-      />
-    </View>
+    />
   );
 }
